1a18347669070560ecc0fc6c9fc0bbf9850198db
UrdoingGood <1222pgy@naver.com>
2024-08-18 14:34:59 +0900

    디저트 카페

--- 디저트 카페/2105.py
+++ 디저트 카페/2105.py
@@ -1,35 +1,31 @@
 import sys
 sys.stdin = open("input.txt", "r")
 
-def dfs(stores, cx, cy, count, chk_num):
+
+def dfs(stores, cx, cy, start_x, start_y, count, chk_num, direction):
     dxy = [(1,1), (1,-1), (-1,-1), (-1,1)] # 상우, 하우, 하좌, 상좌 => 시계방향 사각형으로 돌도록 함
-    chk_num = [] # 방문한 가게의 디저트 숫자를 담아둘 리스트
     global max_count
 
-    visited[cx][cy] = True
+    # 현재 방향(direction)부터 탐색하도록
+    # 한번 수행했던 방향은 탐색하지 않게 됨
+    for i in range(direction, 4):
+        nx, ny = cx + dxy[i][0], cy + dxy[i][1]
 
-    for dx, dy in dxy:
-        nx, ny = cx + dx, cy + dy
-
-        # 사각형이 가장 크면 되는거니까,
         # 범위 넘어가면 다음 방향으로 틀도록
         if nx < 0 or ny < 0 or nx >= N or ny >= N:
             continue
 
         # 처음 위치로 돌아오면 종료
-        if (nx, ny) == (cx, cy):
-            return max_count
+        # 사각형이 되려면 최소 4방향을 돌아야 함
+        if (nx, ny) == (start_x, start_y) and count >= 3:
+            max_count = max(max_count, count + 1) # 디저트 개수 갱신
+            return
 
-        # 이동 가능한 경우
-        visited[nx][ny] = True # 방문 했다고 표시
-        if stores[nx][ny] in chk_num: # 만약 같은 숫자의 디저트를 팔고 있으면 못 감
-            continue
-        chk_num.append(stores[nx][ny]) # 방문한 가게의 디저트 숫자 담기
-        dfs(stores, nx, ny, count, chk_num)
-        max_count = max(max_count, count)
-        visited[nx][ny] = False # 방문 복구
-
-    return max_count
+        # 아직 방문하지 않았으면 갈 수 있음
+        if stores[nx][ny] not in chk_num:
+            chk_num.add(stores[nx][ny]) # 방문한 가게의 디저트 숫자 담기
+            dfs(stores, nx, ny, start_x, start_y, count + 1, chk_num, i)
+            chk_num.remove(stores[nx][ny]) # 방문 복구
 
 
 T = int(input())
@@ -38,11 +34,10 @@ for test_case in range(1, T + 1):
     N = int(input())
     arr = [list(map(int, input().split())) for _ in range(N)] # N*N 크기 디저트 카페
 
-    visited = [[False]*N for _ in range(N)] # 방문 여부 확인할 리스트
-    max_count = 0 # 디저트 최대 종류 수
+    max_count = -1 # 디저트 최대 종류 수, 만들 수 없으면 -1이므로
 
     for i in range(N):
         for j in range(N):
-            result = dfs(arr, i, j, 0, [])
+            dfs(arr, i, j, i, j, 0, set([arr[i][j]]), 0)
 
-    print(f"#{test_case} {result}")
+    print(f"#{test_case} {max_count}")
